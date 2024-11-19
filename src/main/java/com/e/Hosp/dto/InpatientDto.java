27329d998829fe3abb7c3c16f1aeef0465dc239f
praneeth <praneethpk7@gmail.com>
2024-11-19 12:13:28 +0530

    praneeth

---
+++ src/main/java/com/e/Hosp/dto/InpatientDto.java
@@ -0,0 +1,18 @@
+package com.e.Hosp.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InpatientDto {
+    
+
+    
+    private String name;
+    private String addrs;
+}
